8735cd78afe674b74cd2318835dfafd34330a454
Margarida Paulo <63985525+margarida-paulo@users.noreply.github.com>
2024-11-20 16:12:31 +0000

    Outputting to file, change the writes

--- proj_24-25-p1_base/outputFile.c
+++ proj_24-25-p1_base/outputFile.c
@@ -4,7 +4,7 @@ int outputFile(char *diretoria){
 	char ficheiro[strlen(diretoria) + 5];
 	strcpy(ficheiro, diretoria);
 	strcat(ficheiro,"/out");
-	int fd = open(ficheiro, O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	int fd = open(ficheiro, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1){
 		write(STDERR_FILENO, "Couldn't open output file", 26);
 	}
